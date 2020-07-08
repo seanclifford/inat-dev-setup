dc9f3cd2d9ba76d33e31cc0b08ba4e126ea7d6db
Sean Clifford <1187769+seanclifford@users.noreply.github.com>
2020-07-08 15:56:05 +1200

    fixed pull to go in correct directory

--- setup.sh
+++ setup.sh
@@ -2,5 +2,4 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 echo Pulling down inaturalist
-mkdir -p $DIR/../inaturalist
-git -C $DIR/../inaturalist clone https://github.com/inaturalist/inaturalist.git
+git -C $DIR/.. clone https://github.com/inaturalist/inaturalist.git
