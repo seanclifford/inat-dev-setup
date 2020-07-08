5de8ce3b045c89d345d65699361e016915b35447
Sean Clifford <1187769+seanclifford@users.noreply.github.com>
2020-07-08 15:28:14 +1200

    First attempt to pull down repo

--- setup.sh
+++ setup.sh
@@ -1,1 +1,6 @@
-echo test
+#!/usr/bin/env bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+echo Pulling down inaturalist
+mkdir -p $DIR/../inaturalist
+git -C $DIR/../inaturalist clone https://github.com/inaturalist/inaturalist.git
