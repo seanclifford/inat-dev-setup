297c7fbf20ea26e94f6f0bd3794800d85df8b887
Sean Clifford <1187769+seanclifford@users.noreply.github.com>
2021-07-12 11:53:35 +1200

    install correct ruby version

--- setup.sh
+++ setup.sh
@@ -8,5 +8,15 @@ else
   git -C $DIR/.. clone https://github.com/inaturalist/inaturalist.git
 fi
 
+echo installing some ruby build dependencies
+sudo apt-get install -y libssl-dev libreadline-dev zlib1g-dev
+
+echo installing rbenv with rbenv-installer
+wget -q https://github.com/rbenv/rbenv-installer/raw/HEAD/bin/rbenv-installer -O- | bash
+
+echo initialising rbenv
+~/.rbenv/bin/rbenv init
+
 RUBY_VER=$(cat $DIR/../inaturalist/.ruby-version)
-echo installing ruby $RUBY_VER TODO
+echo installing ruby $RUBY_VER with rbenv
+ ~/.rbenv/bin/rbenv install $RUBY_VER
