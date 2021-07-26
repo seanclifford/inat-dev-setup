196633381d598b1ebb2d161c4fd129c9d801c454
Sean Clifford <1187769+seanclifford@users.noreply.github.com>
2021-07-26 17:14:09 +1200

    Get the postgres user and password setup
    Now inaturalist/bin/setup seems to pass

--- postgres_setup.sh
+++ postgres_setup.sh
@@ -1,1 +1,7 @@
-sudo -u postgres -H -- psql -c "CREATE ROLE $USER LOGIN SUPERUSER"
+#!/usr/bin/env bash
+
+sudo -u postgres -H -- psql -c "CREATE ROLE $USER LOGIN SUPERUSER CREATEDB"
+sudo -u postgres -H -- psql -c "ALTER ROLE $USER WITH PASSWORD '$USER'"
+
+echo "*:*:*:$USER:$USER" > ~/.pgpass
+chmod u=rw,og= ~/.pgpass
