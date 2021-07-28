178c77ef0fe44b0dd508b95d5ba95214f51bf2a9
Sean Clifford <1187769+seanclifford@users.noreply.github.com>
2021-07-28 21:54:33 +1200

    changes to make iNaturalistAPI come up (hopefully)

--- inat_setup.sh
+++ inat_setup.sh
@@ -12,15 +12,20 @@ cp docker-compose.override.yml.example docker-compose.override.yml
 #Make sure we use the same username/password for the API db user
 cat ../iNaturalistAPI/docker-compose.override.yml.example | sed 's/username/'$USER'/' | sed 's/password/'$USER'/' > ../iNaturalistAPI/docker-compose.override.yml
 
-#Run the makefile to setup docker services + iNaturalistAPI
-make services-api
+cat ../iNaturalistAPI/config_example.js | sed 's/"username"/"'$USER'"/' | sed 's/"password"/"'$USER'"/' > ../iNaturalistAPI/config.js
 
-#nvm use
+#Run the makefile to setup docker services
+make services
+
+nvm use
 
 # Set up your gems, config files, and database
-#ruby bin/setup
+ruby bin/setup
 
 # Watch the output for errors! In particular, make sure the database gets
 # created correctly. If it doesn't check the settings in config/database.yml
 # Note that you will need to fill out some of the config YML files in config/ to
 # get absolutely everything working, especially third party services.
+
+#Setup iNaturalistAPI with docker (have to get database setup first by bin/setup)
+make services-api
