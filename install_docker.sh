fffb60ddffef9be9cb3aa9eba087c2d9888bfa02
Sean Clifford <1187769+seanclifford@users.noreply.github.com>
2021-08-07 21:19:04 +1200

    Ensure we give the current user docker permissions
    Instead of the root user when run under su

--- install_docker.sh
+++ install_docker.sh
@@ -3,9 +3,10 @@
 echo "installing docker"
 wget -qO- https://get.docker.com | bash
 
-echo "allowing docker to be run by the $USER account without sudo  (requires logout/restart to complete)"
+CURR_USER=$(logname)
+echo "allowing docker to be run by the $CURR_USER account without sudo  (requires logout/restart to complete)"
 groupadd docker
-usermod -aG docker $USER
+usermod -aG docker $CURR_USER
 
 echo "install docker-compose"
 curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
