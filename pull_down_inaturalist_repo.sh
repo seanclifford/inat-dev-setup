d6cb6eab9a8f8560df506109b15e2deb645921a7
Sean Clifford <1187769+seanclifford@users.noreply.github.com>
2021-08-07 21:37:13 +1200

    Move install dependencies to before pulling repos
    So the sudo command is first up

--- pull_down_inaturalist_repo.sh
+++ pull_down_inaturalist_repo.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-echo Pulling down inaturalist
+echo "Pulling down inaturalist"
 if [ -d "$DIR/../inaturalist" ]; then
   git -C $DIR/../inaturalist pull
 else
   git -C $DIR/.. clone https://github.com/inaturalist/inaturalist.git
 fi
 
-echo Pulling down iNaturalistAPI
+echo "Pulling down iNaturalistAPI"
 if [ -d "$DIR/../iNaturalistAPI" ]; then
   git -C $DIR/../iNaturalistAPI pull
 else
--- setup_dependencies.sh
+++ setup_dependencies.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-. pull_down_inaturalist_repo.sh
-
-echo installing some dev dependencies...
+echo "installing some dev dependencies..."
 sudo apt-get install -y libssl-dev libreadline-dev zlib1g-dev libcurl4-openssl-dev libpq-dev libgeos-dev libgeos++-dev libproj-dev postgresql-client-common postgresql-client-12
+
+. pull_down_inaturalist_repo.sh
 
 . install_nodejs.sh
 
