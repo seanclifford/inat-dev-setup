bbc1101fa62e54d3ac8a2d87d70eacf2034a0b09
Sean Clifford <1187769+seanclifford@users.noreply.github.com>
2021-08-15 21:06:54 +1200

    Force nvm to install to a fixed directory

--- install_nodejs.sh
+++ install_nodejs.sh
@@ -3,10 +3,11 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 echo "installing nvm"
+export NVM_DIR="$HOME/.config/.nvm"
+mkdir $NVM_DIR
 wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
 
 #setup nvm command to run right now
-export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
