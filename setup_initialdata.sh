e46147797dd18451e15b86ba4f178faed7e1fb26
Sean Clifford <1187769+seanclifford@users.noreply.github.com>
2021-08-16 20:28:40 +1200

    Add extra users to setup_initialdata
    Plus some other tweaks.

--- setup_initialdata.sh
+++ setup_initialdata.sh
@@ -13,13 +13,17 @@ rails r tools/load_iconic_taxa.rb
 # Generate translation files
 rake inaturalist:generate_translations_js
 
-# Add the basic countries. US states and counties optional... They take up a bit of space
+# Add the basic countries. US counties optional... They take up a bit of space
 rails r tools/import_natural_earth_countries.rb
-#rails r tools/import_us_states.rb
+rails r tools/import_us_states.rb
 #rails r tools/import_us_counties.rb
 
-rails r "User.create( login: 'testerson', password: 'tester', password_confirmation: 'tester', email: 'test@example.com' )"
+rails r "User.create( login: 'testerson', password: 'tester', password_confirmation: 'tester', email: 'test@example.com' )
+User.create( login: 'wednesday', password: 'tester', password_confirmation: 'tester', email: 'wednesday@example.com' )
+User.create( login: 'thursday', password: 'tester', password_confirmation: 'tester', email: 'thursday@example.com' )
+User.create( login: 'friday', password: 'tester', password_confirmation: 'tester', email: 'friday@example.com' )"
+
 rails r tools/load_dummy_observations.rb
 
-echo "run: make services-api in a terminal window to run the API"
-echo "run: rails s -b 0.0.0.0 in a terminal window to run the inaturalist website (accessable on http://127.0.0.1:3000)"
+echo "run: node app.js in a terminal window under iNaturalistAPI to run the API"
+echo "run: rails s -b 0.0.0.0 in a terminal window under inaturalist to run the website (accessable on http://127.0.0.1:3000)"
